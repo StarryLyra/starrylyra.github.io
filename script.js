109953c9486c326e76c40249b33b6a9d2716c47e
Bergen <s.d.van.bergen@umcg.nl>
2026-08-12 14:36:51 +0200

    Redesigned the projects for better understanding

--- script.js
+++ script.js
@@ -2,18 +2,11 @@ const button = document.querySelector("#lang-toggle");
 let currentLang = "nl";
 
 button.addEventListener("click", function() {
-    if (currentLang === "nl") {
-        currentLang = "en";
-        button.textContent = "NL";
-    } else {
-        currentLang = "nl";
-        button.textContent = "EN";
-    }
+    currentLang = currentLang === "nl" ? "en" : "nl";
 
-    const elements = document.querySelectorAll("[data-nl]");
-    elements.forEach(function(el) {
-        el.textContent = el.dataset[currentLang];
-    });
+    document.body.classList.toggle("en", currentLang === "en");
+
+    button.textContent = currentLang === "nl" ? "EN" : "NL";
 });
 
 const emailUser = "svberg";
